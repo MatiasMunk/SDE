84b5c081119ff0b64fe0f0db3be88a1890cb119c
Matias M. Persson <matiaspersson95@gmail.com>
2023-01-24 13:53:47 +0100

    blah blah

--- Assignments/SDE/Yatzy/main.cpp
+++ Assignments/SDE/Yatzy/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include <cstdio>
 #include <cstdarg>
+#include <ctime>
+#include <conio.h>
 #include <string>
 #include <vector>
 #include <map>
@@ -28,12 +31,30 @@ enum Combos
     END_COMBO
 };
 
+std::vector<std::string> explode(char delimiter, const std::string& str)
+{
+    std::size_t lastpos = 0;
+    std::size_t pos = 0;
+    std::vector<std::string> pieces;
+
+    for (pos = str.find_first_of(delimiter); pos != std::string::npos; )
+    {
+        pieces.emplace_back(str.substr(lastpos, pos - lastpos));
+        lastpos = pos + 1;
+        pos = str.find_first_of(delimiter, pos + 1);
+    }
+    pieces.emplace_back(str.substr(lastpos));
+
+    return pieces;
+}
+
 class Player
 {
 private:
 
 public:
     std::string name;
+    std::vector<int> current_hand;
     std::map<unsigned char, int> points;
 
     Player(std::string _name) : name(_name)
@@ -55,16 +76,36 @@ public:
 
         return 0;
     }
+
+    std::vector<unsigned char> ThrowDices(unsigned char _current_dices_in_play)
+    {
+        //Random based on time
+        srand(unsigned(int(time(NULL))));
+
+        std::vector<unsigned char> dices;
+        for (int i = 0; i < _current_dices_in_play; i++)
+        {
+            dices.push_back(1 + rand() % 6);
+        }
+
+        return dices;
+    }
 };
 
-class Sheet
+class Yatzy
 {
 private:
     std::vector<Player*> players;
     std::map<unsigned char, std::string> combo_names;
 
+    unsigned char turn;
+    unsigned char throws;
+    unsigned char current_dices_in_play;
+
 public:
-    Sheet(unsigned char _players, ...)
+    bool winner_found;
+
+    Yatzy(unsigned char _players, ...) : winner_found(false), turn(0), throws(0), current_dices_in_play(5)
     {
         va_list ptr;
         va_start(ptr, _players);
@@ -128,12 +169,124 @@ public:
             ++it;
         }
     }
+
+    void Play()
+    {
+        if (throws != 3)
+        {
+            printf("\n%s's turn\n", this->players.at(turn)->name.c_str());
+            printf("Press Enter to throw the dices\n");
+        }
+
+        char a = _getch();
+        if (a == 13)
+        {
+            for (unsigned char i = 0; i < this->players.size(); i++)
+            {
+                std::vector<unsigned char> dices;
+                if (turn == i)
+                {
+                    dices = this->players.at(i)->ThrowDices(current_dices_in_play);
+                    throws++;
+
+                    printf("%s thew the dices, rolls are: ", this->players.at(i)->name.c_str());
+                    for (int i = 0; i < dices.size(); i++)
+                    {
+                        if (i == dices.size() - 1)
+                            printf("%i\n", dices.at(i));
+                        else
+                            printf("%i, ", dices.at(i));
+                    }
+
+                    char choice;
+                    do
+                    {
+                        printf("\nSave any dice? (Y/N) : ");
+                        choice = _getch();
+                    } while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N');
+                    
+                    if (choice == 'y' || choice == 'Y')
+                    {
+                        std::vector<std::string> save_dice;
+                        std::vector<int> actual_saved_dice;
+                        std::string save_string = "";
+                        
+                        bool saved = false;
+                        while (!saved)
+                        {
+                            printf("\nWhich dice? (comma separated): ");
+                            std::cin >> save_string;
+                            save_dice = explode(',', save_string);
+
+                            if (save_dice.size() > dices.size())
+                            {
+                                printf("You tried to save more dices than the amount that was thrown\n");
+                                continue;
+                            }
+
+                            if (save_dice.size() > 6 - this->players.at(i)->current_hand.size())
+                            {
+                                printf("The amount of dices saved, would surpass the maximum amounts of dices in the game\n");
+                                saved = false;
+                            }
+
+                            for (int attempt_save = 0; attempt_save < save_dice.size(); attempt_save++)
+                            {
+                                for (int thrown_dices = 0; thrown_dices < dices.size(); thrown_dices++)
+                                {
+                                    if (dices.at(thrown_dices) == std::stoi(save_dice.at(attempt_save)))
+                                    {
+                                        actual_saved_dice.push_back(std::stoi(save_dice.at(attempt_save)));
+                                        dices.erase(dices.begin()+thrown_dices);
+                                        break;
+                                    }
+                                }
+                            }
+
+                            saved = true;
+                        }
+
+                        for (int saved_dice = 0; saved_dice < actual_saved_dice.size(); saved_dice++)
+                            this->players.at(i)->current_hand.push_back(actual_saved_dice.at(saved_dice));
+
+                        printf("%s's hand: ", this->players.at(i)->name.c_str());
+                        for (unsigned char hand = 0; hand < this->players.at(i)->current_hand.size(); hand++)
+                        {
+                            if (hand == this->players.at(i)->current_hand.size() - 1)
+                                printf("%i\n", this->players.at(i)->current_hand.at(i));
+                            else
+                                printf("%i, ", this->players.at(i)->current_hand.at(i));
+                        }
+
+                        current_dices_in_play -= actual_saved_dice.size();
+                    }
+                    else if (choice == 'n' || choice == 'N')
+                    {
+                        printf("\nNo dices saved\n");
+                    }
+
+                    
+                    if (throws == 3)
+                    {
+                        printf("%s has thrown 3 times\n", this->players.at(i)->name.c_str());
+
+                        //Save
+                    }
+                }
+            }
+        }
+    }
 };
 
 int main()
 {
-    Sheet* sheet = new Sheet(3, "Matias", "Nicolai", "Casper");
+    Yatzy game(3, "Matias", "Nicolai", "Casper");
 
     printf("Console Yatzy\n");
-    sheet->PrintSheet();
+    game.PrintSheet();
+
+    while (!game.winner_found)
+    {
+        game.Play();
+    }
 }
